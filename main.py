65e140ea849bf61de684b253994cd60bff006283
Ralph McCracken <ralph@royaldestinations.com>
2025-07-02 14:16:17 -0500

    working ish

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import datetime
 
 
 
-
+@st.cache_data
 def smartsheet_to_dataframe(sheet_id):
     smartsheet_client = smartsheet.Smartsheet(st.secrets['smartsheet']['access_token'])
     sheet             = smartsheet_client.Sheets.get_sheet(sheet_id)
@@ -21,7 +21,7 @@ st.set_page_config(page_title='Inspector Schedule', page_icon='🧍🏻‍♀️
 st.image(st.secrets['images']['logo'], width=100)
 
 st.title('Inspector Schedule Assistant')
-st.info('Use occupancy and unit data to help determine turn-day schedules.')
+st.info('Use occupancy, unit, and liaison data to help determine turn-day schedules.')
 
 current_year = datetime.datetime.now().year
 prior_year   = current_year - 1
@@ -35,6 +35,8 @@ if escapia_file is not None:
 
     udf                = smartsheet_to_dataframe(st.secrets['smartsheet']['sheets']['order'])
     adf                = smartsheet_to_dataframe(st.secrets['smartsheet']['sheets']['areas'])
+    
+
     df                 = pd.read_csv(escapia_file)
     df                 = df[['Unit_Code','PropertyName','SleepsMaximum','Bedrooms','Bathrooms','Housekeeper_Name','Reservation_Number','ReservationTypeDescription','Start_Date','Departure']]
     df.columns         = ['Unit_Code','Friendly_Name','SleepsMaximum','Bedrooms','Bathrooms','Housekeeper','Reservation_Number','Reservation_Type','Arrival','Departure']
@@ -65,7 +67,7 @@ if escapia_file is not None:
     udf.Position       = udf.Position.astype(int)
     
     result             = pd.merge(left=turns, right=udf, on=['Unit_Code'], how='left')
-    result             = result[['Unit_Code','Friendly_Name','Address','Sleeps','Bedrooms','Bathrooms','Incoming_Type','Area','Housekeeper','Departing_Reservation_Number','Incoming_Reservation_Number','Position']]
+    result             = result[['Unit_Code','Friendly_Name','Address','Sleeps','Bedrooms','Bathrooms','Incoming_Type','Area','Departing_Reservation_Number','Incoming_Reservation_Number','Position']]
     result             = result.sort_values(by=['Position'])
     result             = result.reset_index(drop=True)
     
@@ -76,37 +78,147 @@ if escapia_file is not None:
 
 
     assign = result
-    assign = assign[['Unit_Code','Friendly_Name','Address','Sleeps','Bedrooms','Bathrooms','Incoming_Type','Area']]
+    assign = assign[['Unit_Code','Friendly_Name','Address','Sleeps','Bedrooms','Bathrooms','Incoming_Type','Area','Position']]
     olhl   = smartsheet_to_dataframe(st.secrets['smartsheet']['sheets']['liaisons'])
+    olhl   = olhl[['Unit_Code','OL','HL']]
     assign = pd.merge(left=assign, right=olhl, on=['Unit_Code'], how='left')
-    assign['Select'] = False
+    assign.insert(0, 'Select', False)
 
     if 'tba' not in st.session_state:
         st.session_state['tba'] = assign
 
-    st.subheader(f'To Be Assigned ({assign.shape[0]})')
-    st.session_state['tba'] = st.data_editor(assign,
-                   column_config={
-                       'Unit_Code': st.column_config.TextColumn(disabled=True),
-                       'Friendly_Name': st.column_config.TextColumn(disabled=True),
-                       'Address': st.column_config.TextColumn(disabled=True),
-                       'Sleeps': st.column_config.NumberColumn(disabled=True),
-                       'Bedrooms': st.column_config.NumberColumn(disabled=True),
-                       'Bathrooms': st.column_config.NumberColumn(disabled=True),
-                       'Incoming_Reservation_Type': st.column_config.TextColumn(disabled=True),
-                       'Area': st.column_config.TextColumn(disabled=True),
-                       'OL': st.column_config.TextColumn(disabled=True),
-                       'HL': st.column_config.TextColumn(disabled=True),
-                       'Select': st.column_config.CheckboxColumn(disabled=False),
-                   },
-                   hide_index=True,
-                   use_container_width=True,
-                   )
+    st.subheader(f'To Be Assigned ({st.session_state['tba'].shape[0]})')
+    selected_df = st.data_editor(
+        st.session_state['tba'],
+        column_config={
+            'Select': st.column_config.CheckboxColumn(disabled=False),
+            'Unit_Code': st.column_config.TextColumn(disabled=True),
+            'Friendly_Name': st.column_config.TextColumn(disabled=True),
+            'Address': st.column_config.TextColumn(disabled=True),
+            'Sleeps': st.column_config.NumberColumn(disabled=True),
+            'Bedrooms': st.column_config.NumberColumn(disabled=True),
+            'Bathrooms': st.column_config.NumberColumn(disabled=True),
+            'Incoming_Reservation_Type': st.column_config.TextColumn(disabled=True),
+            'Area': st.column_config.TextColumn(disabled=True),
+            'Position': st.column_config.NumberColumn(disabled=True),
+            'OL': st.column_config.TextColumn(disabled=True),
+            'HL': st.column_config.TextColumn(disabled=True),
+            },
+            hide_index=True,
+            use_container_width=True,
+            )
 
     l, r = st.columns(2)
 
-    idf = smartsheet_to_dataframe(st.secrets['smartsheet']['sheets']['inspectors'])
+    idf            = smartsheet_to_dataframe(st.secrets['smartsheet']['sheets']['inspectors'])
     idf['Summary'] = idf['Employee'] + ' - ' + idf['Role']
 
-    l.selectbox('Inspector', options=idf.Summary.unique(), label_visibility='collapsed')
-    r.button('Assign', use_container_width=True, type='primary')
+    ols = olhl[['OL']].drop_duplicates()
+    ols.columns = ['Employee']
+    ols['Role'] = 'Owner Liaison'
+    ols['Summary'] = ols['Employee'] + ' - ' + ols['Role']
+
+    hls = olhl[['HL']].drop_duplicates()
+    hls.columns = ['Employee']
+    hls['Role'] = 'Home Liaison'
+    hls['Summary'] = hls['Employee'] + ' - ' + hls['Role']
+
+    idf = pd.concat([idf, ols, hls], ignore_index=True)
+    idf = idf.dropna(subset=['Summary'])
+    idf = idf.sort_values(by=['Employee'])
+
+    selected_inspector = l.selectbox('Inspector', options=idf.Summary.unique(), label_visibility='collapsed')
+    inspector          = selected_inspector.split(' - ')[0]
+
+    selected = selected_df[selected_df['Select'] == True].shape[0]
+
+    if r.button(f'Assign **{selected}** to **{inspector}**', use_container_width=True, type='primary', disabled=selected == 0):
+
+        if selected == 0:
+            st.warning('Please select at least one unit to assign.')
+
+        else:
+        
+            sdf = selected_df[selected_df['Select'] == True].copy()
+            sdf.insert(0, 'Inspector', inspector)
+            sdf.drop(columns=['Select'], inplace=True)
+
+            if 'assigned' not in st.session_state:
+                st.session_state['assigned'] = sdf
+            else:
+                st.session_state['assigned'] = pd.concat([st.session_state['assigned'], sdf], ignore_index=True)
+            
+            st.session_state['tba'] = selected_df[selected_df['Select'] == False].copy()
+            st.rerun()
+
+
+    current_progress = (len(result['Unit_Code'].unique()) - st.session_state['tba'].shape[0]) / len(result['Unit_Code'].unique())
+    st.progress(current_progress)
+
+
+
+    if 'assigned' in st.session_state and not st.session_state['assigned'].empty:
+
+        st.subheader(f'Assigned ({st.session_state["assigned"].shape[0]})')
+
+        original  = st.session_state['assigned']
+        assignees = original.Inspector.sort_values().unique()
+        columns   = st.columns(4)
+        count     = 0
+
+        assignments = original.Inspector.value_counts()
+        average_assignments = assignments.mean()
+
+        for assignee in assignees:
+
+            assignments = original.Inspector.value_counts()
+
+            columns[count].metric(assignee, assignments[assignee], int(assignments[assignee] - average_assignments))
+            count += 1
+                
+            if count == 4: count = 0
+                    
+
+        assigned_df = st.data_editor(
+            st.session_state['assigned'],
+            column_config={
+                'Unit_Code': st.column_config.TextColumn(disabled=True),
+                'Friendly_Name': st.column_config.TextColumn(disabled=True),
+                'Address': st.column_config.TextColumn(disabled=True),
+                'Sleeps': st.column_config.NumberColumn(disabled=True),
+                'Bedrooms': st.column_config.NumberColumn(disabled=True),
+                'Bathrooms': st.column_config.NumberColumn(disabled=True),
+                'Incoming_Type': st.column_config.TextColumn(disabled=True),
+                'Area': st.column_config.TextColumn(disabled=True),
+                'Position': st.column_config.NumberColumn(disabled=True),
+                'Inspector': st.column_config.SelectboxColumn(
+                    options=idf['Employee'].unique(),  
+                ),
+            },
+            hide_index=True,
+            use_container_width=True,
+            )
+        
+        if not original.equals(assigned_df):
+            st.warning('Please save your changes. Not doing so will undo them on the next assignment.')
+            if st.button('Save Changes', use_container_width=True, type='primary'):
+                st.session_state['assigned'] = assigned_df
+                st.rerun()
+        
+        if current_progress == 1:
+
+            final = st.session_state['assigned'].sort_values(['Inspector', 'Position']).copy()
+            final.insert(0, 'Date', date)
+
+            cleaners = df[['Unit_Code','Housekeeper']]
+            cleaners = cleaners.drop_duplicates()
+
+            final = pd.merge(final, cleaners, on='Unit_Code', how='left')
+
+            st.download_button(
+                label=f'Download **Schedule** for **{date.strftime('%A, %m/%d/%y')}**',
+                data=final.to_csv(index=False),
+                file_name=f'Inspectors_{date}.csv',
+                mime='CSV',
+                use_container_width=True,
+                type='primary')
